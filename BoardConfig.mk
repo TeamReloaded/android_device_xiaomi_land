299e95535cbe62cb85e10ea28e21d04ec6b46921
chaubeyprateek <chaubeyprateek@gmail.com>
2017-12-24 05:54:46 +0000

    land: update sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -212,7 +212,7 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/recovery/fstab.qcom
 TARGET_RIL_VARIANT := caf
 
 # SELinux
-#include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/sepolicy.mk
 #BOARD_SEPOLICY_DIRS += \
 #    $(DEVICE_PATH)/sepolicy
 
