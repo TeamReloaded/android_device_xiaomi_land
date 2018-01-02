01b7f84f5a10b25d8325fac7821ed5c5ebf470a6
DeckardShaw <adil.amin820@gmail.com>
2018-01-02 08:03:14 +0000

    land:lineagehw

--- lineagehw/org/lineageos/hardware/KeyDisabler.java
+++ lineagehw/org/lineageos/hardware/KeyDisabler.java
@@ -12,21 +12,34 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */ package org.lineageos.hardware; import org.lineageos.internal.util.FileUtils; /*
+ */
+
+ package org.lineageos.hardware;
+
+ import org.lineageos.internal.util.FileUtils;
+
+/*
+ *
  * Disable capacitive keys
  *
  * This is intended for use on devices in which the capacitive keys
  * can be fully disabled for replacement with a soft navbar. You
  * really should not be using this on a device with mechanical or
  * otherwise visible-when-inactive keys
- */ public class KeyDisabler {
+ */
+
+ public class KeyDisabler {
+
     private static String CONTROL_PATH = "/proc/touchpanel/capacitive_keys_enable";
+
     public static boolean isSupported() {
         return FileUtils.isFileWritable(CONTROL_PATH);
     }
+
     public static boolean isActive() {
         return FileUtils.readOneLine(CONTROL_PATH).equals("0");
     }
+
     public static boolean setActive(boolean state) {
         return FileUtils.writeLine(CONTROL_PATH, state ? "1" : "0");
     }
