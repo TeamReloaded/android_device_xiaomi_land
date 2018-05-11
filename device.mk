a53968623338f7288662656ca8449a5d879757fd
Bruno Martins <bgcngm@gmail.com>
2018-05-11 23:32:31 +0100

    land: Restore audio tuning mixer control
     * After all, this still seems to be needed for ACDB MCS init,
       as seen bellow. Just let ACDB loader be happy.
    
       Without the tuning mixer file:
         E ACDB-MCS: acdb_mcs_init: /vendor/etc/audio_tuning_mixer_tasha.txt not present, using /vendor/etc/audio_tuning_mixer.txt config file
         E MCS-RT-CTL: Can't open the configuration file /vendor/etc/audio_tuning_mixer.txt.
         E ACDB-MCS: acdb_mcs_init: MCS routing control initialization failed.
    
       With the tuning mixer file:
         E ACDB-MCS: acdb_mcs_init: /vendor/etc/audio_tuning_mixer_tasha.txt not present, using /vendor/etc/audio_tuning_mixer.txt config file
         D         : [ACPH]->MCS RTC service registered with ACPH
    
    Change-Id: I6e4b1fe27547a354eaa4132aba8e3df04d6184b3

--- device.mk
+++ device.mk
@@ -90,13 +90,13 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/audio/audio_platform_info_extcodec.xml:system/vendor/etc/audio_platform_info_extcodec.xml \
     $(DEVICE_PATH)/audio/audio_policy.conf:system/vendor/etc/audio_policy.conf \
     $(DEVICE_PATH)/audio/audio_policy_configuration.xml:system/vendor/etc/audio_policy_configuration.xml \
+    $(DEVICE_PATH)/audio/audio_tuning_mixer.txt:system/vendor/etc/audio_tuning_mixer.txt \
     $(DEVICE_PATH)/audio/mixer_paths_qrd_sku1.xml:system/vendor/etc/mixer_paths_qrd_sku1.xml \
     $(DEVICE_PATH)/audio/sound_trigger_mixer_paths.xml:system/vendor/etc/sound_trigger_mixer_paths.xml \
     $(DEVICE_PATH)/audio/sound_trigger_platform_info.xml:system/vendor/etc/sound_trigger_platform_info.xml
 
 # XML Audio configuration files
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/audio/audio_policy_configuration.xml:system/vendor/etc/audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/system/vendor/etc/a2dp_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/vendor/etc/audio_policy_volumes.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/vendor/etc/default_volume_tables.xml \
