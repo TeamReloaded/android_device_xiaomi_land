7626345c7b70452fd5241e5392de72a493315b11
Michael Bestas <mkbestas@lineageos.org>
2018-05-26 21:13:26 +0300

    camera: Link to libhidltransport instead of android.hidl.base
    Change-Id: I7d5de847faf39413290719b2f2d2dc7c56e703da

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -98,6 +98,11 @@ LOCAL_C_INCLUDES += \
 LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl libsync libgui
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder
+ifeq ($(USE_DISPLAY_SERVICE),true)
+LOCAL_SHARED_LIBRARIES += android.frameworks.displayservice@1.0 libhidlbase libhidltransport
+else
+LOCAL_SHARED_LIBRARIES += libgui
+endif
 ifeq ($(TARGET_TS_MAKEUP),true)
 LOCAL_SHARED_LIBRARIES += libts_face_beautify_hal libts_detected_face_hal
 endif
